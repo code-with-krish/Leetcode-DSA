182fe72e080d64b54774c8ef2356a28226d75f65
Krishna Kanta Maiti <krishnakantamaiti7337@gmail.com>
2026-09-20 09:19:47 +0530

    Time: 14 ms (28.09%) | Memory: 46.2 MB (31.24%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        for(int i=0;i<s.length()/2;i++){
+
+            int n = s.length();
+            if (s.charAt(i) != s.charAt(n-1-i)){
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+}
